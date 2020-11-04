c3787e1aef9cbe0ca4ac0ef47c5bccde3743caa9
Sergio Ruiz <60074318+sergio8999@users.noreply.github.com>
2020-11-04 18:26:20 +0100

    Update 1.js

--- Unidad 4/Worksheet 4/1.js
+++ Unidad 4/Worksheet 4/1.js
@@ -1,45 +1,68 @@
 
+/* 
+    1. Fíjate en las 2 siguientes capturas de pantalla:
+    Se trata de la misma página con 2 estilos diferentes (tema normal y tema minimalista).
+    Haciendo sólo uso de JavaScript consigue que se pueda cambiar el estilo (pulsando los
+    enlaces de la parte superior) para que quede tal cual puedes ver. Deberás hacerlo con dos
+    versiones:
+        a) Modificando el estilo desde JavaScript (las hojas de estilo no estan creadas, sino
+        que se establece mediante atributos del objeto style).
+        b) Creando dos hojas de estilo diferentes que se carguen dinámicamente según la
+        opción pulsada.
+*/
 
-function main(){
-    var temaNormalHTML = document.getElementById('textoNormal');
-    var temaMinimalistaHTML = document.getElementById('textoMinimalista');
+
+function main(){    
+    let temaMinimalistaHTML = document.getElementById('textoMinimalista');
+    let temaNormalHTML = document.getElementById('textoNormal');
     temaNormal();
     temaNormalHTML.addEventListener('click',temaNormal);
     temaMinimalistaHTML.addEventListener('click',temaMinimalista);
 }
 
+// a)
+function temaNormal(){   
+    let divElegirTema = document.getElementById('elegirTema');
+    let temaNormalHTML = document.getElementById('textoNormal');
+    let temaMinimalistaHTML = document.getElementById('textoMinimalista');
+    let divMarco = document.getElementById('marco');
+    let parrafoOculto = document.getElementById('parrafoOculto');
 
-
-function temaNormal(){
-    document.getElementById('elegirTema').style.backgroundColor = "lightblue";
-    document.getElementById('textoNormal').style.color = "blue";
-    document.getElementById('textoMinimalista').style.color = "blue";
-    document.getElementById('textoNormal').style.fontWeight = "bold";
-    document.getElementById('textoMinimalista').style.fontWeight = "bold";
-    document.getElementById('textoNormal').style.textDecoration = "none";
-    document.getElementById('textoMinimalista').style.textDecoration = "none";
-    document.getElementById('elegirTema').style.borderStyle = "solid";
+    divElegirTema.style.backgroundColor = "lightblue";
+    temaNormalHTML.style.color = "blue";
+    temaMinimalistaHTML.style.color = "blue";
+    temaNormalHTML.style.fontWeight = "bold";
+    temaMinimalistaHTML.style.fontWeight = "bold";
+    temaNormalHTML.style.textDecoration = "none";
+    temaMinimalistaHTML.style.textDecoration = "none";
+    divElegirTema.style.borderStyle = "solid";
     document.body.style.backgroundColor = "#fdfd96";
     document.body.style.fontSize = "16px";
-    document.getElementById('marco').style.backgroundColor = "yellow";
-    document.getElementById('marco').style.borderStyle = "solid";
-    document.getElementById('parrafoOculto').style.display = "inline";
+    divMarco.style.backgroundColor = "yellow";
+    divMarco.style.borderStyle = "solid";
+    parrafoOculto.style.display = "inline";
 }
 
 function temaMinimalista(){
-    document.getElementById('elegirTema').style.backgroundColor = "white";
-    document.getElementById('textoNormal').style.color = "blue";
-    document.getElementById('textoMinimalista').style.color = "blue";
-    document.getElementById('textoNormal').style.fontWeight = "normal";
-    document.getElementById('textoMinimalista').style.fontWeight = "normal";
-    document.getElementById('textoNormal').style.textDecoration = "underline";
-    document.getElementById('textoMinimalista').style.textDecoration = "underline";
-    document.getElementById('elegirTema').style.borderStyle = "none";
+    let divElegirTema = document.getElementById('elegirTema');
+    let temaNormalHTML = document.getElementById('textoNormal');
+    let temaMinimalistaHTML = document.getElementById('textoMinimalista');
+    let divMarco = document.getElementById('marco');
+    let parrafoOculto = document.getElementById('parrafoOculto');
+
+    divElegirTema.style.backgroundColor = "white";
+    temaNormalHTML.style.color = "blue";
+    temaMinimalistaHTML.style.color = "blue";
+    temaNormalHTML.style.fontWeight = "normal";
+    temaMinimalistaHTML.style.fontWeight = "normal";
+    temaNormalHTML.style.textDecoration = "underline";
+    temaMinimalistaHTML.style.textDecoration = "underline";
+    divElegirTema.style.borderStyle = "none";
     document.body.style.backgroundColor = "white";
     document.body.style.fontSize = "20px";
-    document.getElementById('marco').style.backgroundColor = "white";
-    document.getElementById('marco').style.borderStyle = "none";
-    document.getElementById('parrafoOculto').style.display = "none";
+    divMarco.style.backgroundColor = "white";
+    divMarco.style.borderStyle = "none";
+    parrafoOculto.style.display = "none";
 }
 
 window.addEventListener('load',main);
