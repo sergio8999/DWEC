36cad2133d3230672722b749ecfaa8f2fa134262
Sergio Ruiz <60074318+sergio8999@users.noreply.github.com>
2020-12-14 18:06:38 +0100

    update

--- Unidad 5/Workshhet 5 - stickyNotes/stickyNotes.js
+++ Unidad 5/Workshhet 5 - stickyNotes/stickyNotes.js
@@ -1,108 +1,159 @@
+import{Vista, VistaNota1, VistaNota2} from "./clases.js";
+
 var notas = {"listaNotas":[]};  // Crear array vacion JSON
-var titulo,descripcion,fecha,contenedorDiv,notaMovimiento,sections;
+var vista = new Vista();
+var vistaNota = new VistaNota1();
 var arrastrando = false;
+var notaMovimiento,cont;
 
 function main(){
-
-    contenedorDiv = document.getElementById('divNotas');
-    titulo = document.getElementById('titulo');
-    descripcion = document.getElementById('descripcion');
-
+    elegirVista();
     cargarJSON();
+    // contador para saber id de etiquetas
+    cont = notas.listaNotas.length;
+    crearNota();
+    enventoMover();
+
+    // Cada 10 segundos obtiene y actualiza el tiempo que lleva creada las notas
+    setInterval(obtenerMinutos,10000);
+}
 
 
-    // Crear nota al pulsar el boton
-    document.getElementById('botonEnviar').addEventListener('click',()=>{ 
-        fecha = Date.now();
-        notas.listaNotas.push({"titulo":titulo.value,"descripcion":descripcion.value,"fecha":fecha})
-        let nota = notas.listaNotas[notas.listaNotas.length-1];
-        dibujarNota(nota);
-        document.getElementsByTagName('section')[notas.listaNotas.length-1].addEventListener('click',notaPulsada);
-        limpiarTexto();
-        localStorage.setItem("listaNotas",JSON.stringify(notas.listaNotas));
+function elegirVista(){
+    document.getElementById('vista1').addEventListener('click',()=>{
+        borrarNotas();
+        vista = new Vista();
+        vistaNota = new VistaNota1();
+        localStorage.setItem("vistaNota",JSON.stringify("vista1"));
+        cont = notas.listaNotas.length;
+        cambiarEstilo();
+        obtenerMinutos();
     });
 
-    sections = document.querySelectorAll('section');
+    document.getElementById('vista2').addEventListener('click',()=>{
+        /* divNotas.innerHTML=""; */
+        borrarNotas();
+        vista = new Vista();
+        vistaNota = new VistaNota2();
+        localStorage.setItem("vistaNota",JSON.stringify("vista2")); 
+        cont = notas.listaNotas.length;
+        cambiarEstilo();
+        obtenerMinutos();
+    });
+}
+
+function borrarNotas(){
+    let section = document.getElementsByTagName('section');
+    for(let i=section.length-1;i>=0;i--){
+        section[i].remove();
+    }
+}
+
+function cambiarEstilo(){
+    for(let i=0;i<notas.listaNotas.length;i++){
+        vistaNota.dibujarNota(notas.listaNotas[i],i);
+        borrarNota(i);
+        editarNota(i);
+    }
+    enventoMover();
+}
+
+function enventoMover(){
+    let sections = document.querySelectorAll('section');
     sections.forEach(section => section.addEventListener('click',notaPulsada));
     window.addEventListener('mousemove',moverNota);
-
-
-
-
-   /*  notas.listaNotas.splice(1,1);
-    document.getElementsByTagName('section')[1].remove(); */
 }
 
+function crearNota(){
+    let titulo = document.getElementById('titulo');
+    let descripcion = document.getElementById('descripcion');
+
+    document.getElementById('botonEnviar').addEventListener('click',()=>{ 
+        let fecha =Date.now();
+        notas.listaNotas.push({"titulo":titulo.value,"descripcion":descripcion.value,"fechaMilisegundos":fecha,"fechaDigitos":obtenerFechaCreacion()});
+        let nota = notas.listaNotas[notas.listaNotas.length-1];
+        vistaNota.dibujarNota(nota,cont);
+        document.getElementById('sectionNota'+cont).addEventListener('click',notaPulsada);
+        borrarNota(cont);
+        editarNota(cont);
+        vista.limpiarTexto();
+        localStorage.setItem("listaNotas",JSON.stringify(notas.listaNotas)); 
+        cont++;
+    });
+}
+
+function borrarNota(id){
+    document.getElementById('divBotones'+id).lastChild.addEventListener('click',()=>{
+        notas.listaNotas.splice(id,1);
+        vista.borrarNota(id);
+        localStorage.setItem("listaNotas",JSON.stringify(notas.listaNotas));
+    })
+}
+
+function editarNota(id){
+    document.getElementById('divBotones'+id).firstChild.addEventListener('click',()=>{
+        let titulo = document.getElementById('tituloNota'+id).value;
+        let descripcion = document.getElementById('descripcionNota'+id).value;
+        notas.listaNotas[id].titulo = titulo;
+        notas.listaNotas[id].descripcion = descripcion;
+        localStorage.setItem("listaNotas",JSON.stringify(notas.listaNotas));
+    });
+}
+
+function obtenerMinutos(){
+    let minutos;
+    for(let i=0;i<notas.listaNotas.length;i++){
+        minutos = parseInt((Date.now()-notas.listaNotas[i].fechaMilisegundos)/1000/60);
+        vista.dibujarMinutos(i,minutos);
+    }
+}
+
+function obtenerFechaCreacion(){
+    let fecha = new Date();
+    return fecha.getDate() + "/" + (fecha.getMonth() +1) + "/" + fecha.getFullYear();
+}
 
 function notaPulsada(e){
-    arrastrando = !arrastrando;
-    notaMovimiento = e.currentTarget;
+    if(e.target.localName=="section"){
+        arrastrando=!arrastrando;
+        notaMovimiento=e.currentTarget;
+    }
 }
-
 function moverNota(e){
     let contenedorDiv = document.getElementById('divNotas').getBoundingClientRect();
-    let sectionNotas = document.querySelector('section').getBoundingClientRect();
-    if(arrastrando){
-        if(e.y > contenedorDiv.y && e.x < (contenedorDiv.width-sectionNotas.width)){
-            notaMovimiento.style.left = e.x /* - 20  */+ "px";
-            notaMovimiento.style.top = e.y /* - 20 */+ "px";
+    let sectionNotas = document.querySelector('section');
+
+    if(sectionNotas !=null){
+        if(arrastrando){
+            if(e.y > contenedorDiv.y && e.x < (contenedorDiv.width-sectionNotas.getBoundingClientRect().width)){
+                notaMovimiento.style.left = e.x /* - 20  */+ "px";
+                notaMovimiento.style.top = e.y /* - 20 */+ "px";
+            }
         }
     }
 }
 
- function dibujarNota(nota){
-    // Crear id
-    let id = notas.listaNotas.length-1;
-
-    let section = document.createElement('section');
-    let labelTitulo = document.createElement('label');
-    labelTitulo.textContent = "Titulo:";
-    labelTitulo.setAttribute('for','tituloNota'+id);
-
-    let input = document.createElement('input');
-    input.value = nota.titulo;
-    input.id = "tituloNota"+id;
-
-    let divTextArea = document.createElement('div');
-    divTextArea.style.display = "flex";
-    divTextArea.style.flexDirection = "column";
-
-    let labelDescripcion = document.createElement('label');
-    labelDescripcion.textContent = "Descripcion:";
-    labelDescripcion.setAttribute('for','descripcionNota'+id);
-
-    let textArea = document.createElement('textarea');
-    textArea.textContent = nota.descripcion;
-    textArea.id = "descripcionNota"+id;
-
-    let parrafo = document.createElement('p');
-    parrafo.textContent="fecha";
-
-    //Añadir a div
-    contenedorDiv.appendChild(section);
-    section.appendChild(labelTitulo);
-    section.appendChild(input);
-    section.appendChild(divTextArea);
-    divTextArea.appendChild(labelDescripcion);
-    divTextArea.appendChild(textArea);
-    section.appendChild(parrafo);
-}
-
-
-function limpiarTexto(){
-    titulo.value = "";
-    descripcion.value = "";
-}
-
 function cargarJSON(){
     let notasJSON = JSON.parse(localStorage.getItem('listaNotas'));
-    if(notasJSON != null)
-    /* for(let i=0;i<notasJson.length;i++) */
-    for(let notaJSON of notasJSON){
-     //let nota = new Nota(notasJson[i].titulo,notasJson[i].descripcion);
-     notas.listaNotas.push(notaJSON);
-     dibujarNota(notaJSON);
+    let estilo = JSON.parse(localStorage.getItem('vistaNota'));
+
+    if(estilo == "vista1")
+        vistaNota = new VistaNota1();
+    else if(estilo == "vista2"){
+        vistaNota = new VistaNota2();
+        document.getElementById('vista2').checked= true;
+    }
+
+    if(notasJSON != null){
+        for(let i=0;i<notasJSON.length;i++){
+            notas.listaNotas.push(notasJSON[i]);
+            vistaNota.dibujarNota(notasJSON[i],i); 
+            borrarNota(i);
+            editarNota(i);
+        }
+        obtenerMinutos();
     }
 }
+
 
 window.addEventListener('load',main);
